37d419198278e1e2fe3f6d26ea5beb74694b5db0
TsuBenn <TsuBenn@github.com>
2025-11-06 11:40:50 +0700

    fold

--- after/plugin/fold.lua
+++ after/plugin/fold.lua
@@ -3,43 +3,11 @@ vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decr
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
-local function fold_virt_text(result, s, lnum, coloff)
-    if not coloff then
-        coloff = 0
-    end
-    local text = ""
-    local hl
-    for i = 1, #s do
-        local char = s:sub(i, i)
-        local hls = vim.treesitter.get_captures_at_pos(0, lnum, coloff + i - 1)
-        local _hl = hls[#hls]
-        if _hl then
-            local new_hl = "@" .. _hl.capture
-            if new_hl ~= hl then
-                table.insert(result, { text, hl })
-                text = ""
-                hl = nil
-            end
-            text = text .. char
-            hl = new_hl
-        else
-            text = text .. char
-        end
-    end
-    table.insert(result, { text, hl })
-end
-
 function _G.custom_foldtext()
-    local start = vim.fn.getline(vim.v.foldstart):gsub("\t", string.rep(" ", vim.o.tabstop))
-    local num_lines = vim.v.foldend - vim.v.foldstart + 1
-    local end_str = vim.fn.getline(vim.v.foldend)
-    local end_ = vim.trim(end_str)
-    local result = {}
-    fold_virt_text(result, start, vim.v.foldstart - 1)
-    table.insert(result, { " ... ", "Delimiter" })
-    fold_virt_text(result, end_, vim.v.foldend - 1, #(end_str:match("^(%s+)") or ""))
-    fold_virt_text(result, " ("..num_lines.." lines)", vim.v.foldend)
-    return result
+    local startline = vim.fn.getline(vim.v.foldstart):match("^%s*(.-)%s*$")
+    local numline = vim.v.foldend - vim.v.foldstart + 1
+    local endline = vim.fn.getline(vim.v.foldend):match("^%s*(.-)%s*$")
+    return {{startline, "@keyword"}, {" ... ", "Delimiter"}, {endline, "@keyword"}, {" ("..numline.." lines)", "FoldLineCount"}}
 end
 
 vim.opt.foldtext = "v:lua.custom_foldtext()"
