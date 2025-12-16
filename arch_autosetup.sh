b1eb769be62f8752dd9dee58a6d159f79e449e91
TsuBenn <TsuBenn@Github.com>
2025-12-16 11:20:59 +0700

    Linux: added Libre Office

--- arch_autosetup.sh
+++ arch_autosetup.sh
@@ -111,7 +111,7 @@ GAMING_PKGS=(
 
 
 ESSENTIAL_APPS=(
-    discord obs-studio 7zip zoxide filelight ark mpv yazi dolphin
+    discord obs-studio 7zip zoxide filelight ark mpv yazi dolphin libreoffice
     vlc vlc-plugin-x264 vlc-plugin-ffmpeg lame libmad pinta openrgb github-cli
     flatpak avahi
     rofi rofi-emoji
